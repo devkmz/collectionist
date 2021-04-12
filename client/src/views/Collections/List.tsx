10b439c79e265d1dba6531ded99a381ef8083833
Karolina Jasiak <kjasiak1993@gmail.com>
2021-04-12 20:57:39 +0200

    [CL-50] add confirm modal for deleting collection

--- client/src/views/Collections/List.tsx
+++ client/src/views/Collections/List.tsx
@@ -1,5 +1,5 @@
-import { Breadcrumb, Button, Card, Col, Empty, message, Row, Spin, Tooltip } from 'antd';
-import { EditOutlined, DeleteOutlined, EyeOutlined } from '@ant-design/icons';
+import { Breadcrumb, Button, Card, Col, Empty, message, Modal, Row, Spin, Tooltip } from 'antd';
+import { EditOutlined, DeleteOutlined, EyeOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
 import axios from 'axios';
 import { css, SerializedStyles } from '@emotion/core';
 import React, { useEffect, useState } from 'react';
@@ -11,6 +11,7 @@ import { Collection } from '../../types/collection';
 import { User } from '../../types/user';
 
 const { Meta } = Card;
+const { confirm } = Modal;
 
 interface Props {
     user?: User;
@@ -19,6 +20,17 @@ interface Props {
 const styles = (): SerializedStyles => css`
     .ant-card {
         position: relative;
+
+        .ant-card-actions {
+            > li {
+                margin: 0;
+                padding: 0;
+            }
+
+            .action-wrapper {
+                padding: 12px 0;
+            }
+        }
     }
 `;
 
@@ -27,18 +39,40 @@ const CollectionList = ({ user }: Props): JSX.Element => {
     const [isLoading, setIsLoading] = useState(false);
     const { t } = useTranslation();
 
-    useEffect(() => {
-        const getCollections = async () => {
-            try {
-                setIsLoading(true);
-                const response = await axios.get('http://localhost:8000/api/collections');
-                setData(response.data);
-            } catch (error) {
-                message.error(`${error}`);
-            } finally {
-                setIsLoading(false);
+    const getCollections = async () => {
+        try {
+            setIsLoading(true);
+            const response = await axios.get('http://localhost:8000/api/collections');
+            setData(response.data);
+        } catch (error) {
+            message.error(`${error}`);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    const deleteCollection = (id: number, name: string) => {
+        confirm({
+            title: t('collections.list.delete-collection'),
+            icon: <ExclamationCircleOutlined />,
+            content:
+                <>
+                    <div>{ t('collections.list.delete-confirm') }</div>
+                    <div>{ name }?</div>
+                </>,
+            onOk: async () => {
+                try {
+                    await axios.delete(`http://localhost:8000/api/collections/${id}`);
+                    message.success(t('collections.list.delete-success'));
+                    getCollections();
+                } catch (error) {
+                    message.error(t('common.messages.something-went-wrong'));
+                }
             }
-        };
+        });
+    };
+
+    useEffect(() => {
         getCollections();
     }, []);
 
@@ -71,22 +105,40 @@ const CollectionList = ({ user }: Props): JSX.Element => {
                                         {
                                             data.map(item => (
                                                 <Col span={6} key={`item-${item.id}`}>
+                                                    { console.log(item) }
                                                     <Link to={`/collections/${item.id}`}>
                                                         <Card
                                                             cover={<CardImage imageUrl={item.imageUrl} />}
                                                             actions={user?.role === 'ADMIN' ? [
                                                                 <Tooltip title={t('common.actions.view')}>
-                                                                    <EyeOutlined key="view" onClick={(e) => e.preventDefault() } />
+                                                                    <Link to={`/collections/${item.id}`}>
+                                                                        <div className="action-wrapper">
+                                                                            <EyeOutlined key="view" />
+                                                                        </div>
+                                                                    </Link>
                                                                 </Tooltip>,
                                                                 <Tooltip title={t('common.actions.edit')}>
-                                                                    <EditOutlined key="edit" onClick={(e) => e.preventDefault() } />
+                                                                    <div
+                                                                        className="action-wrapper"
+                                                                        onClick={(e) => e.preventDefault()}
+                                                                    >
+                                                                        <EditOutlined key="edit" />
+                                                                    </div>
                                                                 </Tooltip>,
                                                                 <Tooltip title={t('common.actions.delete')}>
-                                                                    <DeleteOutlined key="delete" onClick={(e) => e.preventDefault() } />
+                                                                    <div
+                                                                        className="action-wrapper"
+                                                                        onClick={(e) => {
+                                                                            e.preventDefault();
+                                                                            deleteCollection(item.id, item.name);
+                                                                        }}
+                                                                    >
+                                                                        <DeleteOutlined key="delete"  />
+                                                                    </div>
                                                                 </Tooltip>
                                                             ] : [
-                                                                <Button type="link">{ t('common.actions.view') }</Button>
-                                                            ]}
+                                                                    <Button type="link">{t('common.actions.view')}</Button>
+                                                                ]}
                                                         >
                                                             <Meta
                                                                 title={item.name}
@@ -99,8 +151,8 @@ const CollectionList = ({ user }: Props): JSX.Element => {
                                         }
                                     </Row>
                                 ) : (
-                                    <Empty />
-                                )
+                                        <Empty />
+                                    )
                             }
                         </>
                     )

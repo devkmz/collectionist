2bee1e5acf4b1b3e9c0114d1aa835852db4d4fb3
Karolina Jasiak <kjasiak1993@gmail.com>
2021-05-10 18:04:50 +0200

    Change storage type for user token

--- client/src/UserContext.tsx
+++ client/src/UserContext.tsx
@@ -1,11 +1,14 @@
+import { message } from 'antd';
 import axios from 'axios';
 import React, { createContext, useContext, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
 import { User } from './types/user';
 
 export type UserContextType = {
     user: User | undefined;
-    registerUser: (email: string, password: string, confirmPassword: string, onSuccess: () => void) => Promise<void>;
-    loginUser: (email: string, password: string, onSuccess: () => void) => Promise<void>;
+    registerUser: (email: string, password: string, confirmPassword: string) => Promise<void>;
+    loginUser: (email: string, password: string) => Promise<void>;
     loadUser: () => Promise<void>;
     clearUser: () => void;
 }
@@ -14,29 +17,20 @@ export const UserContext = createContext<UserContextType | null>(null);
 
 export const UserProvider: React.FC<React.ReactNode> = ({ children }) => {
     const [user, setUser] = useState<User | undefined>(undefined);
+    const { t } = useTranslation();
 
-    const registerUser = async (email: string, password: string, confirmPassword: string, onSuccess: () => void) => {
-        try {
-            await axios.post('http://localhost:8000/api/register', { email, password, password_confirmation: confirmPassword });
-            onSuccess();
-        } catch (error) {
-            console.log(error);
-        }
+    const registerUser = async (email: string, password: string, confirmPassword: string) => {
+        await axios.post('http://localhost:8000/api/register', { email, password, password_confirmation: confirmPassword });
     };
 
-    const loginUser = async (email: string, password: string, onSuccess: () => void) => {
-        try {
-            const response = await axios.post('http://localhost:8000/api/login', { email, password });
-            localStorage.setItem('collectionist-token', response.data.token);
-            onSuccess();
-        } catch (error) {
-            console.log(error);
-        }
+    const loginUser = async (email: string, password: string) => {
+        const response = await axios.post('http://localhost:8000/api/login', { email, password });
+        sessionStorage.setItem('collectionist-token', `Bearer ${response.data.token}`);
     };
 
     const loadUser = async () => {
         try {
-            const token = localStorage.getItem('collectionist-token');
+            const token = sessionStorage.getItem('collectionist-token');
 
             if (!token) {
                 setUser(undefined);
@@ -47,17 +41,18 @@ export const UserProvider: React.FC<React.ReactNode> = ({ children }) => {
                 method: 'get',
                 url: 'http://localhost:8000/api/user',
                 headers: {
-                    'Authorization': `Bearer ${token}`
+                    'Authorization': token
                 }
             });
-            setUser({ ...userDetails.data.user, role: 'ADMIN' });
+            setUser(userDetails.data.user);
         } catch (error) {
+            message.error(t('common.messages.error'));
             console.log(error);
         }
     };
 
     const clearUser = () => {
-        localStorage.removeItem('collectionist-token');
+        sessionStorage.removeItem('collectionist-token');
         setUser(undefined);
     };
 

fb6317b7b2289f3c6bba6b5bd8176c6b27e29fe5
Karolina Jasiak <kjasiak1993@gmail.com>
2021-05-28 19:24:34 +0200

    [CL-92] Add axios interceptor for attaching authorization header

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 import 'antd/dist/antd.css';
 import plPL from 'antd/lib/locale/pl_PL';
 import { ConfigProvider } from 'antd';
+import axios from 'axios';
 import moment from 'moment';
 import 'moment/locale/pl';
 import React from 'react';
@@ -13,6 +14,19 @@ import './index.css';
 import './i18n';
 
 moment.locale('pl');
+
+axios.interceptors.request.use(request => {
+  const token = sessionStorage.getItem('collectionist-token');
+
+  if (token) {
+    request.headers.common['Authorization'] = token;
+  } else {
+    request.headers.common['Authorization'] = null;
+  }
+  return request;
+}, error => {
+  return Promise.reject(error);
+});
 
 ReactDOM.render(
   <UserProvider>

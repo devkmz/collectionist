f0e78da9195184f5d3539d82f48cba2b2b1efe8a
Karolina Jasiak <kjasiak1993@gmail.com>
2021-05-28 20:00:32 +0200

    Add reducer and actions for updating user after profile update

--- client/src/context/actions.ts
+++ client/src/context/actions.ts
@@ -1,5 +1,36 @@
 import axios from 'axios';
 
+export const updateUser = async (dispatch: any) => {
+  try {
+    dispatch({ type: "CURRENT_USER_UPDATE_INIT" });
+    const token = sessionStorage.getItem('collectionist-token');
+
+    if (!token) {
+      throw new Error();
+    }
+
+    const userResponse = await axios({
+      method: 'get',
+      url: 'http://localhost:8000/api/user',
+      headers: {
+          'Authorization': token
+      }
+    });
+
+    if (!userResponse.data) {
+      throw new Error();
+    }
+
+    const { user } = userResponse.data;
+    sessionStorage.setItem('collectionist-user', JSON.stringify(user));
+    dispatch({ type: "CURRENT_USER_UPDATE_SUCCESS", payload: user });
+    return user;
+  } catch (error) {
+    dispatch({ type: "CURRENT_USER_UPDATE_ERROR", error: error });
+    return false;
+  }
+};
+
 export const register = async (dispatch: any, payload: any) => {
   try {
     dispatch({ type: "REGISTER_INIT" });
